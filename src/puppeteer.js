421070fce7c519984f407f6839825ad81e72ab9c
Irish Fagan <IrishFagan@live.com>
2020-10-19 15:03:40 -0700

    Initial commit. Add google sign in functionality

---
+++ src/puppeteer.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+const googleSignIn = () => {
+	puppeteer.use(StealthPlugin())
+	
+	puppeteer.launch({headless: false}).then(async browser => {
+		console.log("Working...")
+		const page = await browser.newPage()
+		await page.goto('https://accounts.google.com/signin/v2/identifier?sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+
+		await page.waitFor(2000)
+		await page.$eval('input[type=email]', el => el.value = 'coolguy@gmail.com'/*username here*/)
+		await page.keyboard.press('Enter')
+		await page.waitFor(2000)
+		await page.$eval('input[type=password]', el => el.value = '123'/*password here*/)
+		await page.keyboard.press('Enter')
+		await page.screenshot({path: 'result.png', fullPage: true})
+	
+		await browser.close()
+	})
+}
+
+googleSignIn()
