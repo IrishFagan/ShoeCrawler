a81bbb2b8597ea88f738e2958e8a34f18a7dc432
Irish Fagan <IrishFagan@live.com>
2020-10-19 16:20:22 -0700

    Add SignIn component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import SignIn from './components/SignIn.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Shoe Scraper</h1>
+      <SignIn />
     </div>
   );
 }
