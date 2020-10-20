30ae7b4c3be6ada6ea44f510022675c760cb26d6
Irish Fagan <IrishFagan@live.com>
2020-10-19 19:02:04 -0700

    Add Task component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import SignIn from './components/SignIn.js'
+import Task from './components/Task.js'
 
 function App() {
   return (
     <div className="App">
       <h1>Shoe Scraper</h1>
       <SignIn />
+      <h2>Tasks</h2>
+      <Task />
     </div>
   );
 }
