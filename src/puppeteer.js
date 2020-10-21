aa05f80cc680631a4b09231ee50acdfc0889d028
Irish Fagan <IrishFagan@live.com>
2020-10-20 23:17:08 -0700

    Revise googleSignIn functionality to swap browser to headfull while signing in

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -1,40 +1,38 @@
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+
 const app = express()
 const port = 3005
 
 app.use(bodyParser.json())
 app.use(cors({origin: true}))
+puppeteer.use(StealthPlugin())
 
-const googleSignIn = () => {
-	puppeteer.use(StealthPlugin())
-	
-	puppeteer.launch({headless: false}).then(async browser => {
-		console.log("Working...")
-		const page = await browser.newPage()
-		await page.goto('https://accounts.google.com/signin/v2/identifier?sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+const googleSignIn = async (browser) => {
+	browser = await puppeteer.launch({headless: false})
 
-		await page.waitFor(2000)
-		await page.$eval('input[type=email]', el => el.value = 'coolguy@gmail.com'/*username here*/)
-		await page.waitFor(2000)
-		await page.keyboard.press('Enter')
-		await page.waitFor(2000)
-		await page.$eval('input[type=password]', el => el.value = '123'/*password here*/)
-		await page.keyboard.press('Enter')
-		await page.waitFor(10000)
-		const cookies = await page.cookies()
+	var page = await browser.newPage()
+	await page.goto('https://accounts.google.com/signin/v2/identifier?sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
+	await page.waitFor(3500)
+	await page.$eval('input[type=email]', el => el.value = 'coolguy@gmail.com'/*username here*/)
+	await page.waitFor(2000)
+	await page.keyboard.press('Enter')
+	await page.waitFor(2000)
+	await page.$eval('input[type=password]', el => el.value = '123'/*password here*/)
+	await page.keyboard.press('Enter')
+	await page.waitFor(10000)
+	const cookies = await page.cookies()
+	await browser.close()
 
-		const page2 = await browser.newPage()
-		await page2.setCookie(...cookies)
-		await page2.goto('http://google.com')
-	
-		await page2.waitFor(100000)
-		await browser.close()
-	})
+	browser = await puppeteer.launch({headless: true})
+	page = await browser.newPage()
+	await page.setCookie(...cookies)
+	await page.goto('https://www.google.com')
+	await page.screenshot({path: 'signed_in.png'})
+	return browser
 }
 
 const spawnTask = body => {
@@ -47,7 +45,7 @@ const spawnTask = body => {
 }
 
 app.get('/signin/', (req, res) => {
-	googleSignIn()
+	googleSignIn(puppeteer.launch({headless: true}))
 	console.log('-- Signed into Google --')
 })
 
