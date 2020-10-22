d558c244c42435afa49ebc0bd68255234f1ab57a
Irish Fagan <IrishFagan@live.com>
2020-10-21 21:28:02 -0700

    Change functionality to spawn new window to sign in on through electron

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,24 +1,17 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+const { ipcRenderer } = window.require('electron')
 
 const SignIn = () => {
   const handleSubmit = event => {
-    console.log('cool')
-    event.preventDefault()
-    axios.get(`http://localhost:3005/signin/`)
-      .then(res => {
-        console.log(res)
-      })
-      .catch(e => {
-        console.log(e)
-      })
+    ipcRenderer.send('open-captcha')
   }
 
 	return(
 		<div>
-		  <label>Sign In to Google
+		  <label>Open Captcha Window
         <form onSubmit={handleSubmit}>
-          <button type="submit">Sign In</button>
+          <button type="submit">Open Captcha</button>
         </form>
       </label>
 		</div>
