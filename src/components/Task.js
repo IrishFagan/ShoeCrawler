1949b5b2aae3e5efe06c59eb38383cc3249648c3
Irish Fagan <IrishFagan@live.com>
2020-10-22 17:26:46 -0700

    Change content displayed

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,10 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 
-const Task = () => {
-  const [site, setSite] = useState('')
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    axios.post(`http://localhost:3005/task`,
-      {site: site})
-    setSite('')
-  }
+const Task = ({ taskContent }) => {
 
 	return(
 		<div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={site} onChange={e => setSite(e.target.value)} />
-        <input type="submit" />
-      </form>
+      <p>{taskContent}</p>
 		</div>
 	)
 }
