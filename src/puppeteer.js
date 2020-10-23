9386aefcf0d610a781c5429d0bc06ccda7013d5a
Irish Fagan <IrishFagan@live.com>
2020-10-23 00:33:06 -0700

    Add getSupremeStock function to get available product json

--- src/puppeteer.js
+++ src/puppeteer.js
@@ -3,6 +3,7 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 const port = 3005
@@ -16,6 +17,10 @@ app.use(express.json())
 const returnHeadfull = async (browser) => {
 	await browser.close()
 	return await puppeteer.launch({headless: false})
+}
+
+const getSupremeStock = () => {
+	return axios.get('http://www.supremenewyork.com/mobile_stock.json')
 }
 
 const spawnTask = body => {
