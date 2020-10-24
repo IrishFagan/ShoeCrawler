978203ae78e96a4241a8e8450086353512edbfdd
Irish Fagan <IrishFagan@live.com>
2020-10-23 17:11:04 -0700

    Move styling to css file. Adjust styling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
 import React, { useState } from 'react';
 import SignIn from './components/SignIn.js'
-import Task from './components/Task.js'
+import TaskWindow from './components/TaskWindow.js'
+import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [tasks, setTasks] = useState([])
-  const [taskField, setTaskField] = useState('')
-
-  const handleTaskSubmit= e => {
-    e.preventDefault()
-    const taskObject = {
-      site: taskField,
-      date: new Date().toISOString()
-    }
-
-    axios
-      .post('http://localhost:3005/tasks/',
-        taskObject)
-      .then(response => {
-        setTasks(tasks.concat(response.data))
-        setTaskField('')
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
-  document.body.style.margin = "0"
-  document.body.style.overflowY = "hidden"
-
-  const divStyle = {
-    backgroundColor: "#4d4d4d",
-    height: "100vh",
-    margin: "0px",
-  }
 
   return (
-    <div style={divStyle} className="App">
-      <div style={{padding: "10px"}}>
-        <h1>Shoe Scraper</h1>
-        <SignIn /><br />
-        <form onSubmit={handleTaskSubmit}>
-          <input type="text" value={taskField} onChange={e => setTaskField(e.target.value)} />
-          <input type="submit" />
-        </form>
-        <ul>
-          {tasks.map(task => 
-            <Task key={task.id} taskContent={task.site} />
-          )}
-        </ul>
-        <Task />
+    <div className="App">
+      <div style={{padding: "10px", height: "90%"}}>
+        <TaskWindow />
+        <SignIn />
       </div>
     </div>
   );
 }
-
-
 
 export default App;
